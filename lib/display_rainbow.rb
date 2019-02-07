a9e26c7b80b61b00d27b3f69b616ea11fea6e03a
abstract-animation-9914 <vm930@hotmail.com>
2019-02-07 14:56:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,10 @@
 # Write your #display_rainbow method here
 def display_rainbow(array_colors)
-  puts "R: #{array_colors[0]}, "
-  puts "O: #{array_colors[1]}, "
-  puts "Y: #{array_colors[2]}, "
-  puts "G: #{array_colors[3]}, "
-  puts "B: #{array_colors[4]}, "
-  puts "I: #{array_colors[5]}, "
+  puts "R: #{array_colors[0]}, \n"
+  puts "O: #{array_colors[1]}, \n"
+  puts "Y: #{array_colors[2]}, \n"
+  puts "G: #{array_colors[3]}, \n"
+  puts "B: #{array_colors[4]}, \n"
+  puts "I: #{array_colors[5]}, \n"
   puts "V: #{array_colors[6]}"
 end 
